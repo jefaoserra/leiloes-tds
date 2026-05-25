b09ff24f63d343388731b12ee5acc245ac441f3d
jefaoserra <jefaoserra2016@gmail.com>
2026-05-25 10:28:38 -0300

    Implementado cadastro de produtos no banco de dados

--- src/cadastroVIEW.java
+++ src/cadastroVIEW.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -140,16 +143,28 @@ public class cadastroVIEW extends javax.swing.JFrame {
     }//GEN-LAST:event_cadastroNomeActionPerformed
 
     private void btnCadastrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadastrarActionPerformed
-        ProdutosDTO produto = new ProdutosDTO();
-        String nome = cadastroNome.getText();
-        String valor = cadastroValor.getText();
-        String status = "A Venda";
-        produto.setNome(nome);
-        produto.setValor(Integer.parseInt(valor));
-        produto.setStatus(status);
-        
-        ProdutosDAO produtodao = new ProdutosDAO();
-        produtodao.cadastrarProduto(produto);
+try {
+        if (cadastroNome.getText().trim().isEmpty() || cadastroValor.getText().trim().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Preencha todos os campos.");
+            return;
+        }
+
+        ProdutosDTO objprodutosdto = new ProdutosDTO();
+
+        objprodutosdto.setNome(cadastroNome.getText());
+        objprodutosdto.setValor(Integer.parseInt(cadastroValor.getText()));
+        objprodutosdto.setStatus("A Venda");
+
+        ProdutosDAO objprodutosdao = new ProdutosDAO();
+        objprodutosdao.cadastrarProduto(objprodutosdto);
+
+        cadastroNome.setText("");
+        cadastroValor.setText("");
+
+    } catch (NumberFormatException erro) {
+        JOptionPane.showMessageDialog(null, "Digite um valor numérico válido.");
+    }
+
         
     }//GEN-LAST:event_btnCadastrarActionPerformed
 
