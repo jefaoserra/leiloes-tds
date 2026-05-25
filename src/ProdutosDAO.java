73bae16bb720cb4197ced176e7c3a39b037d8d23
jefaoserra <jefaoserra2016@gmail.com>
2026-05-25 13:59:04 -0300

    Implementado método para listar produtos vendidos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -82,4 +82,34 @@ public class ProdutosDAO {
         JOptionPane.showMessageDialog(null, "Erro ao vender produto: " + erro.getMessage());
     }
 }
+    public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+    ArrayList<ProdutosDTO> vendidos = new ArrayList<>();
+
+    String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+
+    conn = new conectaDAO().connectDB();
+
+    try {
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
+
+        while (resultset.next()) {
+            ProdutosDTO produto = new ProdutosDTO();
+
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+
+            vendidos.add(produto);
+        }
+
+        prep.close();
+
+    } catch (Exception erro) {
+        JOptionPane.showMessageDialog(null, "Erro ao listar produtos vendidos: " + erro.getMessage());
+    }
+
+    return vendidos;
+}
 }
